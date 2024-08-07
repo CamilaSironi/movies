caed213005451f078ce883adab2dc00857117df1
Camila Sironi <sironicamila98@gmail.com>
2024-08-07 13:18:27 -0300

    k

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,8 @@ app.use(express.json());
 
 //Paths to routers:
 
+app.get('/', (req, res) => res.status(200).json({message: "Hello"}))
+
 app.use('/api/v1/movies', movieRouter);
 
 module.exports = app;
