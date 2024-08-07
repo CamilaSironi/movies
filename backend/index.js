3ef9e1bba9302191f99151f3a881aa27139f41e0
Camila Sironi <sironicamila98@gmail.com>
2024-08-06 22:44:09 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:["https://movies-black-ten.vercel.app/"],
+        origin:["https://movies-front-five.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
