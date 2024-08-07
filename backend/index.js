921e308a4197b8d7e98d5caaa9b65c4dd23416cb
Camila Sironi <sironicamila98@gmail.com>
2024-08-07 12:41:54 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors(
 app.use(express.json());
 
 //Paths to routers:
-
+app.get('/', (req, res) => res.status(200).json({message: "Hello World"});
 app.use('/api/v1/movies', movieRouter);
 
 module.exports = app;
